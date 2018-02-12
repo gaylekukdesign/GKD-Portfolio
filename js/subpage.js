e92676819fb150ba4dd32afd366bbf2f532a4f2f
Katie Seo <seo.katie@gmail.com>
2018-02-11 22:20:23 -0800

    no animation if it is from subpages

---
+++ js/subpage.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#go-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    $(".scroll").on('click', function(event) {
+        window.location.hash = hash;
+    });
+});
